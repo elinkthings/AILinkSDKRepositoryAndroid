3cb34a6f52de81a5628b5510ae8c3c8f541575a2
陈福行 <chenfuxing@elinkthings.com>
2023-08-17 16:24:10 +0800

    update jar

--- app/src/main/java/aicare/net/cn/sdk/ailinkrepositoryandroid/MainActivity.java
+++ app/src/main/java/aicare/net/cn/sdk/ailinkrepositoryandroid/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.pingwang.bluetoothlib.AILinkBleManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
