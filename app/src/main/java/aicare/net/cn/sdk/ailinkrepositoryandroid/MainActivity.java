73a2c08c3a3f7b2e38e5d71bdcd1a86938f8e35e
陈福行 <chenfuxing@elinkthings.com>
2023-10-10 12:45:13 +0800

    update jar

--- app/src/main/java/aicare/net/cn/sdk/ailinkrepositoryandroid/MainActivity.java
+++ app/src/main/java/aicare/net/cn/sdk/ailinkrepositoryandroid/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.pingwang.bluetoothlib.AILinkBleManager;
 
 public class MainActivity extends AppCompatActivity {
 
